3e8f159aa9af8e8c0d09f29640ca5049308c0f07
Gaucho <juanmcastillo3@gmail.com>
2022-07-26 20:51:21 -0300

    Actualización de Display

--- index.js
+++ index.js
@@ -3,5 +3,12 @@ const displayValorActual = document.getElementById('valor-actual');
 const botonesNumeros = document.querySelectorAll('.numero');
 const botonesOperadores = document.querySelectorAll('.operador');
 
+const display = new Display(displayValorAnterior, displayValorActual);
 
-const Calculadora = new Calculadora();
+botonesNumeros.forEach(boton => {
+    boton.addEventListener('click', () => display.agregarNumero(boton.innerHTML));
+});
+
+botonesOperadores.forEach(boton => {
+    boton.addEventListener('click', () => display.computar(boton.value))
+});
